a7d118007fc05718ae9c0345361e7cc5cbb94cb9
Cornelia28 <75266999+Cornelia28@users.noreply.github.com>
2021-02-10 11:49:34 +0700

    Add files via upload

---
+++ conditionn_if/src/ganjil_genap/java/ganjil.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ganjil_genap.java;
+
+/**
+ *
+ * @author Neli
+ */
+public class ganjil {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO code application logic here
+        int angka = 21;
+        if (angka %2 == 0)
+            System.out.println(angka + " adalah bilangan genap ");
+        else
+            System.out.println(angka + " adalah bilangan ganjil ");
+    }
+}
